915ca54799111e8fb5731b86571b977bb400ff69
tanishqcodes <tanishqcodes@gmail.com>
2024-05-15 22:26:00 +0530

    f

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const { spawn } = require('child_process');
 
-exports.handler = async (event, context) => {
+module.exports = async (req, res) => {
   const command = 'dig google.com';
   const child = spawn(command, { shell: true });
   let output = '';
 
-  return new Promise((resolve, reject) => {
-    child.stdout.on('data', (data) => {
-      output += data.toString();
-    });
+  child.stdout.on('data', (data) => {
+    output += data.toString();
+  });
 
-    child.stderr.on('data', (data) => {
-      output += data.toString();
-    });
+  child.stderr.on('data', (data) => {
+    output += data.toString();
+  });
 
-    child.on('close', (code) => {
-      resolve({ statusCode: 200, body: output });
-    });
+  child.on('close', (code) => {
+    res.status(200).send(output);
+  });
 
-    child.on('error', (err) => {
-      reject({ statusCode: 500, body: err.message });
-    });
+  child.on('error', (err) => {
+    res.status(500).send(err.message);
   });
 };
