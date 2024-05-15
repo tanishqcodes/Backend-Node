09709afa698242902e1a451364a1f9241aa85948
tanishqcodes <tanishqcodes@gmail.com>
2024-05-15 22:10:48 +0530

    new commit

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const os = require('os');
+const { spawn } = require('child_process');
 
-module.exports = async (req, res) => {
-  console.log('Operating System:', os.platform());
-  res.status(200).send(`Operating System: ${os.platform()}`);
+exports.handler = async (event, context) => {
+  const command = 'dig google.com';
+  return new Promise((resolve, reject) => {
+    const child = spawn(command, { shell: true });
+
+    let output = '';
+    child.stdout.on('data', (data) => {
+      output += data.toString();
+    });
+
+    child.stderr.on('data', (data) => {
+      output += data.toString();
+    });
+
+    child.on('close', (code) => {
+      resolve({ statusCode: 200, body: output });
+    });
+
+    child.on('error', (err) => {
+      reject({ statusCode: 500, body: err.message });
+    });
+  });
 };
